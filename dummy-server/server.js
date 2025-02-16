ebfdc88f4898900284b7eaea8f9be3dc83a6f177
Sharonk77 <sharon.kroch@gmail.com>
2025-02-16 16:51:56 +0200

    created a simple dummy server

---
+++ dummy-server/server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(cors()); // Enable CORS
+
+app.get("/health", (req, res) => {
+    res.status(200).send("OK");
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
