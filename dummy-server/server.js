4f302cf7b4355ed98b08bfe0f1362f7d3b078fb0
Sharonk77 <sharon.kroch@gmail.com>
2025-02-17 23:58:38 +0200

    f

--- dummy-server/server.js
+++ dummy-server/server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 80;
 
 app.use(cors()); // Enable CORS
 
