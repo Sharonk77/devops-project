514149d734a2dc00c2feaef669b20f4743d08f13
Sharonk77 <sharon.kroch@gmail.com>
2025-02-17 12:45:36 +0200

    fix

--- dummy-server-tf/ecs-deploy.tf
+++ dummy-server-tf/ecs-deploy.tf
@@ -139,5 +139,8 @@ resource "aws_ecs_service" "dummy-service" {
   launch_type     = "FARGATE"
 
   network_configuration {
-    subnets          = [aws_subnet.public_subnet.id]
-    security_groups  = [aws_security_group.ecs_sg.id]
+    subnets = [aws_subnet.public_subnet.id]
+    security_groups = [aws_security_group.ecs_sg.id]
+
+  }
+}
