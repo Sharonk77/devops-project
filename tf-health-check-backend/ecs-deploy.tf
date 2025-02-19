33b6d5e676bf2c22405c77792794ed848a10e23f
Sharonk77 <sharon.kroch@gmail.com>
2025-02-19 20:39:05 +0200

    f

--- tf-health-check-backend/ecs-deploy.tf
+++ tf-health-check-backend/ecs-deploy.tf
@@ -190,7 +190,7 @@ resource "aws_ecs_service" "service" {
   iam_role        = aws_iam_role.ecs_task_execution_role.arn
 
   load_balancer {
-    target_group_arn = ecs_tg
+    target_group_arn = aws_lb_target_group.ecs_tg.arn
     container_name   = "health-check-backend"
     container_port   = 80
   }
