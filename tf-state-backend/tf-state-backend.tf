226fc7a157753e787b6ed811d3f447b9c9fbd35f
Sharonk77 <sharon.kroch@gmail.com>
2025-02-19 18:14:24 +0200

    f

--- tf-state-backend/tf-state-backend.tf
+++ tf-state-backend/tf-state-backend.tf
@@ -22,7 +22,7 @@ resource "aws_s3_bucket_versioning" "terraform_bucket_versioning" {
 }
 
 resource "aws_s3_bucket_server_side_encryption_configuration" "terraform_state_crypto_conf" {
-  bucket        = aws_s3_bucket.terraform_state.bucket
+  bucket = aws_s3_bucket.terraform_state.bucket
   rule {
     apply_server_side_encryption_by_default {
       sse_algorithm = "AES256"
