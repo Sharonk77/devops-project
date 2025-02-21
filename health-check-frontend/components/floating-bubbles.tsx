4bf2ebf3214333481e40b0857882d98e895a6877
Sharonk77 <sharon.kroch@gmail.com>
2025-02-21 15:17:38 +0200

    f

--- health-check-frontend/components/floating-bubbles.tsx
+++ health-check-frontend/components/floating-bubbles.tsx
@@ -63,7 +63,7 @@ export default function FloatingBubblesBackground({
     useEffect(() => {
         const checkServerStatus = async () => {
             try {
-                const response = await fetch(process.env.NEXT_PUBLIC_API_URL as string)
+                const response = await fetch('${process.env.NEXT_PUBLIC_API_URL}/health')
                 if (response.ok) {
                     setServerStatus("up")
                 } else {
