fbf3ae38aa22813bd7d9eb8a77a59c36ec78a970
Sharonk77 <sharon.kroch@gmail.com>
2025-02-23 00:50:29 +0200

    f

--- tf-health-check-backend/ecs-deploy-outputs.tf
+++ tf-health-check-backend/ecs-deploy-outputs.tf
@@ -1,4 +1,22 @@
 output "load_balancer_url" {
-  description = "The Load Balancer URL"
   value       = aws_lb.ecs_lb.dns_name
 }
+
+output "task_name" {
+  description = "extract task name"
+  value       = element(split(":", element(split("/", aws_ecs_service.service.task_definition), 1)), 0)
+}
+
+output "service_name" {
+  value       = aws_ecs_service.service.name
+}
+
+output "cluster_name" {
+  value       = aws_ecs_cluster.ecs_cluster.name
+}
+
+output "container_name" {
+  value = jsondecode(aws_ecs_task_definition.health-check-backend-task.container_definitions)[0].name
+}
+
+
